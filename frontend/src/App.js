5b4195fe2eda50a57b917cbb153bd6ce86c8923f
christian <cfs.bsi@outlook.com>
2017-10-30 23:02:47 -0200

    chore: Setup the project be tested and to use bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {ListGroup, ListGroupItem, Panel, Col} from 'react-bootstrap';
+import {Route, Link} from 'react-router-dom';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Route exact path="/" render={() => (
+                    <Col md="8" mdOffset="2">
+                        <Panel header="Redux">
+                            <ListGroup>
+                                <ListGroupItem>Link 1</ListGroupItem>
+                                <ListGroupItem>Link 2</ListGroupItem>
+                            </ListGroup>
+                        </Panel>
+                    </Col>
+                )}/>
+            </div>
+        );
+    }
 }
 
 export default App;
